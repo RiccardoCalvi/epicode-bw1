7994cad92679065000e35cf9854ac4c23a842fd8
domenicofrau <domenico.frau@outlook.com>
2022-11-16 11:34:56 +0100

    RISULTATI => aggiunto con punteggi

--- src/js/risultati.js
+++ src/js/risultati.js
@@ -5,3 +5,37 @@ let sbagliate = domande - corrette
 console.log(corrette)
 console.log(domande)
 console.log(sbagliate)
+
+let percentualeCorrette = Math.floor((corrette / domande)*100)
+console.log(percentualeCorrette)
+
+
+
+
+
+let myCanvas = document.querySelector("#myCanvas").getContext('2d');
+let myLabels = ["Wrong", "Correct"];
+let myData = [sbagliate, corrette];
+
+let chart = new Chart(myCanvas, {
+    type: 'doughnut',
+    data: {
+        labels: myLabels,
+        datasets: [{
+            label: "Punteggio",
+            data: myData,
+            backgroundColor: ['#D20094', '#00FFFF'],
+            hoverBorderColor: '#000000',
+            hoverBorderWidth: 1,
+            hoverOffset: 2,
+            cutout: '70%',
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+});
